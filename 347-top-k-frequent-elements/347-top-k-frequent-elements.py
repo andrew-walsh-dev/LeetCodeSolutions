011244e655392d2b9383e89c250baafd713cc66e
Andrew Walsh <78042294+andrew-walsh-dev@users.noreply.github.com>
2022-08-05 08:31:26 -0500

    Time: 144 ms (44.76%), Space: 20.7 MB (30.30%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -5,20 +5,17 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        if len(nums) == 1:
-            return nums
         
         count = {}
         freq = [[] for i in range(len(nums) + 1)]
         
         for num in nums:
-            count[num] = count.get(num, 0) + 1 
-        
-        for number, count in count.items():
-            freq[count].append(number)
+            count[num] = count.get(num, 0) + 1
+        for num, count in count.items():
+            freq[count].append(num)
         res = []
         for i in range(len(freq) - 1, 0, -1):
             for num in freq[i]:
                 res.append(num)
-                if len(res) == k:
-                    return res
+            if k == len(res):
+                return res
